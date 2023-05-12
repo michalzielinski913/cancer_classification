ce16c4697975329f95573eed9a155feaf7e50779
danielmarek22 <danielmarek22@gmail.com>
2023-05-12 18:06:01 +0200

    fix training loop and add model evaluation

--- deepClassifier.py
+++ deepClassifier.py
@@ -11,6 +11,30 @@ from torch.utils.data import Dataset, DataLoader
 #Utilities from sklearn
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report
+
+class BinaryClassification(nn.Module):
+    def __init__(self):
+        super(BinaryClassification, self).__init__()
+        # Number of input features is 16
+        self.layer_1 = nn.Linear(16, 10) 
+        self.layer_2 = nn.Linear(10, 10)
+        self.layer_out = nn.Linear(10, 1) 
+        
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.batchnorm1 = nn.BatchNorm1d(10)
+        self.batchnorm2 = nn.BatchNorm1d(10)
+        
+    def forward(self, inputs):
+        x = self.relu(self.layer_1(inputs))
+        x = self.batchnorm1(x)
+        x = self.relu(self.layer_2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.layer_out(x)
+        
+        return x
 
 class TrainData(Dataset):
     
@@ -35,34 +59,66 @@ class TestData(Dataset):
     def __len__ (self):
         return len(self.X_data)
 
-def trainingLoop(train_loader):
-    model = nn.Sequential(
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.Linear(16, 2),
-        nn.Softmax(dim=1)
-    )
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.round(torch.sigmoid(y_pred))
+
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]
+    acc = torch.round(acc * 100)
+    
+    return acc
+
+def trainingLoop(train_loader, model):
+
 
     learning_rate = 1e-2
-    optimizer = optim.SGD(model.parameters(), lr = learning_rate)
-    loss_fn = nn.NLLLoss()
 
-    n_epochs = 100
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    n_epochs = 200
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     for epoch in range(n_epochs):
+        epoch_loss = 0
+        epoch_acc = 0
         for features, label in train_loader:
+            label = label.unsqueeze(1)
             # features, label = features.to(device), label.to(device)
-
+            
+            # print(len(features))
+            # print(len(label))
+            # print(features.shape)
+            # print(label.shape)
+            # print(type(features))
+            # print(type(label))
             features = features.to(torch.float32)
             outputs = model(features)
-            loss = loss_fn(outputs, label)
+
+            loss = criterion(outputs, label)
+            acc = binary_acc(outputs, label)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        print("Epoch: %d, Loss: %f" % (epoch, float(loss)))
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+            print(f'Epoch {epoch+0:03}: | Loss: {epoch_loss/len(train_loader):.5f} | Acc: {epoch_acc/len(train_loader):.3f}')
+
+def test_model(test_loader, model):
+    y_pred_list = []
+    model.eval()
+    with torch.no_grad():
+        for X_batch in test_loader:
+            # X_batch = X_batch.to(device)
+            y_test_pred = model(X_batch)
+            y_test_pred = torch.sigmoid(y_test_pred)
+            y_pred_tag = torch.round(y_test_pred)
+            y_pred_list.append(y_pred_tag.cpu().numpy())
+
+    y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+    return y_pred_list
 
 
 
@@ -80,12 +136,18 @@ print(y.shape)
 # Split the dataset into train and validation sets
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-train_data = TrainData(torch.LongTensor(X_train.to_numpy()), 
-                       torch.LongTensor(y_train.to_numpy()))
+train_data = TrainData(torch.FloatTensor(X_train.to_numpy()), 
+                       torch.FloatTensor(y_train.to_numpy()))
 
-test_data = TestData(torch.LongTensor(X_val.to_numpy()))
+test_data = TestData(torch.FloatTensor(X_val.to_numpy()))
 
 train_loader = DataLoader(dataset=train_data, batch_size=10, shuffle=True)
 test_loader = DataLoader(dataset=test_data, batch_size=1)
 
-trainingLoop(train_loader)
+model = BinaryClassification()
+
+trainingLoop(train_loader, model)
+preds = test_model(test_loader, model)
+
+print(confusion_matrix(y_val, preds))
+# print(classification_report(y_val, preds))
